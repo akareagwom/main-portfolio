f0f324a5e55a98141f8374341186942c4ed9321a
akareagwom <akareazikang20@gmail.com>
2025-10-26 22:41:46 +0100

    navbar start

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+const Navbar = () => {
+  return (
+    <div className="bg-[#000000D9] flex justify-center flex-col items-center w-[40%] rounded-full text-white p-4">
+      <div className="flex justify-between w-full">
+        <h1>LOGO</h1>
+        <ul className="flex gap-2">
+          <li>
+            <a href="http://">Home</a>
+          </li>
+          <li>
+            <a href="http://">Contact</a>
+          </li>
+          <li>
+            <a href="http://">Experience</a>
+          </li>
+        </ul>
+        <div className="flex">
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
