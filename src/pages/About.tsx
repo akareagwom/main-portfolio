e93c685ab02298b409227d094a4da6f5c34ed4c1
akareagwom <akareazikang20@gmail.com>
2025-11-13 23:00:25 +0100

    about section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,32 @@
-const About =()=>{
-    return(
-        <div className="p-10 text-gray-600 md:p-16 lg:p-20">
-            <p>Hello! I'm a passionate and skilled frontend developer with a robust background in creating responsive and user-friendly interfaces. My journey into the world of web development has been fueled by my love for technology and creativity. With a solid foundation in React.js, HTML, CSS, and JavaScript, I thrive on turning complex problems into elegant, user-centric solutions.</p>
-        </div>
-    )
-}
+const About = () => {
+  return (
+    <div className="p-10 text-gray-600 md:p-16 leading-[24px] flex flex-col gap-4 lg:p-20">
+      <h1 className="text-3xl font-xl text-black">About</h1>
+      <p>
+        Hello! I'm a passionate and skilled frontend developer with a robust
+        background in creating responsive and user-friendly interfaces. My
+        journey into the world of web development has been fueled by my love for
+        technology and creativity. With a solid foundation in React.js, HTML,
+        CSS, and JavaScript, I thrive on turning complex problems into elegant,
+        user-centric solutions.
+      </p>
+      <p>
+        My journey as a developer started from a deep curiosity about how
+        technology can be harnessed to solve real-world problems. I have worked
+        on projects ranging from e-learning platforms to audio recording
+        interfaces, honing my skills in creating seamless user experiences. My
+        focus is on writing clean, maintainable code, while ensuring
+        accessibility and performance are prioritized.
+      </p>
+      <h1>Key Responsibilties</h1>
+      <div>
+        <li>Building and Maintaining Web Applications</li>
+        <li>Clean Code Practices</li>
+        <li>Performance and User Experience</li>
+        <li>Collaborating with product and design teams to turn user needs and designs into functional applications</li>
+      </div>
+    </div>
+  );
+};
 
 export default About;
